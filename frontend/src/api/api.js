98011510201e7200865baeaefe218e5a85a4fddd
Sabari Rohith Viswa Senthilkumar <sabarirohiths@clouddestinations.com>
2026-08-21 20:55:20 +0530

    update in ui

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
-const API_BASE_URL = `http://${window.location.hostname}:8000/api/v1`;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `http://${window.location.hostname}:8000/api/v1`;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
